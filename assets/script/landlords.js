e5d96134bfa4da1939622dcd19bfcd05057264b4
JACKLINEKARIUKI <jackline.kariuki@student.moringaschool.com>
2022-07-20 23:18:38 +0300

    create js file

---
+++ assets/script/landlords.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+document.getElementById("button").addEventListener("click", function(){
+    document.getElementById("popup").style.display = "flex";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("popup").style.diplay = "none";
+})
