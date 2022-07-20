fb78d5d7f1107e917fb4a9d9c7d36c656145eb2c
JACKLINEKARIUKI <jackline.kariuki@student.moringaschool.com>
2022-07-21 02:30:26 +0300

    add property details into object

--- assets/script/landlords.js
+++ assets/script/landlords.js
@@ -1,3 +1,70 @@
+let county, constituency, city, streetAddress, propertyType, propertyTypeValue, size, buildingName, apartmentNo, bedrooms, bathrooms, availability, availabilityValue, landlord, contactPerson, contactPhoneNumber, rent, deposit;
+
+
+county = document.getElementById("county");
+constituency = document.getElementById("constituency");
+city = document.getElementById("city");
+streetAddress = document.getElementById("streetAddress");
+propertyType = document.getElementById("propertyType");
+propertyTypeValue = select.options[select.selectedIndex].value;
+size = document.getElementById("size");
+buildingName = document.getElementById("buildingName");
+apartmentNo = document.getElementById("apartmentNo");
+bedrooms = document.getElementById("bedrooms");
+bathrooms = document.getElementById("bathrooms");
+availability = document.getElementById("availability");
+availabilityValue = select.options[select.selectedIndex].value;
+landlord = document.getElementById("landlord");
+contactPerson = document.getElementById("contactPerson");
+contactPhoneNumber = document.getElementById("contactPhoneNumber");
+rent = document.getElementById("rent");
+deposit = document.getElementById("deposit");
+
+
+propertyDetails = []
+
+function addProperty() {
+    propertyDetails.push({
+        county: county.value,
+        constituency: constituency.value,
+        city: city.value,
+        streetAddress: streetAddress.value,
+        propertyType: propertyTypeValue,
+        size: size.value,
+        buildingName: buildingName.value,
+        apartmentNo: apartmentNo.value,
+        bedrooms: bedrooms.value,
+        bathrooms: bathrooms.value,
+        availability: availabilityValue,
+        landlord: landlord.value,
+        contactPerson: contactPerson.value,
+        contactPhoneNumber: contactPhoneNumber.value,
+        rent: rent.value,
+        deposit: deposit.value
+    })
+    console.log(propertyDetails)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
